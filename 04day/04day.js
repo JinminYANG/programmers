0f1293f8e78a05e3c9a9cd38b14dd8c168adf55a
JinminYANG <38518818+JinminYANG@users.noreply.github.com>
2020-09-20 00:30:35 +0900

    lv.1 - 문자열 내 p와 y의 개수

--- 04day/04day.js
+++ 04day/04day.js
@@ -1,25 +1,25 @@
-function solution(s){
+function solution(s) {
     var text = s.toLowerCase();
     var answer = true;
 
-    var arr  = new Array();
+    var arr = new Array();
 
-    for(var i =0; i<text.length; i++){
+    for (var i = 0; i < text.length; i++) {
         arr[i] = text.charAt(i);
     }
     var p_count = 0;
     var y_count = 0;
-    for(var k =0; k<arr.length; k++){
-        if(arr[k]=="p"){
+    for (var k = 0; k < arr.length; k++) {
+        if (arr[k] == "p") {
             p_count += 1;
-        }else if(arr[k] =="y"){
+        } else if (arr[k] == "y") {
             y_count += 1;
         }
     }
 
-    if(p_count == y_count){
+    if (p_count == y_count) {
         answer = true;
-    }else{
+    } else {
         answer = false;
     }
     console.log(answer);
